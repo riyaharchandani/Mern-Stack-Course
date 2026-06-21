bfbabe9a65db9f435e228e5cadbaeea3d3d91375
riyaharchandani <riyaharchandani76@gmail.com>
2026-06-21 20:03:48 +0530

    javascript

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,185 @@
-console.log("Hello World!");
-let a = 5;
-let b = 10;
-console.log("sum is :", a + b);
+// console.log("Hello World!");
+// let a = 5;
+// let b = 10;
+// console.log("sum is :", a + b);
+
+// let pencilPrice = 10;
+// let erasorPrice = 5;
+// console.log(`The total price is : ${pencilPrice + erasorPrice} Rupees.`);
+
+// Arithmetic Operator
+// let a = 10;
+// let b = 15;
+// console.log("ans is :", a + b);
+// console.log("ans is :", a - b);
+// console.log("ans is :", a * b);
+// console.log("ans is :", a / b);
+// console.log("ans is :", a % b);
+// console.log("ans is :", a ** b);
+
+// Unary Operator
+// console.log("post-increment :", a++);
+// console.log("pre-increment :", ++a);
+
+// Assignment Operator
+// b = a;
+// console.log(b);
+
+//Conditional Statements(IF)
+// console.log("before my if statement :");
+// let age = 23;
+// if (age >= 18) {
+//   console.log("you can vote");
+// }
+
+// console.log("after my if statement :");
+// if (age > 20) {
+//   console.log("you're in your 20's");
+// }
+
+//Conditional Statements
+// let firstName = "Riya";
+// if (firstName == "Riya") {
+//   console.log(`welcome ${firstName}`);
+// }
+
+//traffic light system (ELSE IF)
+// let color = "green";
+// if (color === "red") {
+//   console.log("Stop! light color is red");
+// } else if (color === "yellow") {
+//   console.log("Slow Down, light color is yellow");
+// } else if (color === "green") {
+//   console.log("Go. light color is green");
+// }
+
+// let age = 14;
+// if (age >= 18) {
+//   console.log("you can vote");
+// } else if (age >= 18) {
+//   console.log("you cannot vote");
+// } else if (age < 18) {
+//   console.log("you can vote");
+// }
+
+// let marks = 75;
+// if (marks >= 80) {
+//   console.log("A+");
+// } else if (marks >= 60) {
+//   console.log("A");
+// } else if (marks >= 33) {
+//   console.log("B");
+// } else if (marks < 33) {
+//   console.log("F");
+// }
+
+//ELSE
+// let age = 17;
+// if (age >= 18) {
+//   console.log("you can vote");
+// } else {
+//   console.log("you cannot vote");
+// }
+
+//PRACTICE QUESTION
+// let size = "S";
+// if (size === "XL") {
+//   console.log("price is Rs.", 250);
+// } else if (size === "L") {
+//   console.log("price is Rs.", 200);
+// } else if (size === "M") {
+//   console.log("price is Rs.", 100);
+// } else console.log("price is Rs.", 50);
+
+// Nested If
+// let marks = 32;
+
+// if (marks >= 33) {
+//   console.log("Pass");
+//   if (marks >= 80) {
+//     console.log("Grade:O");
+//   } else console.log("Grade: F");
+// } else {
+//   console.log("Better luck next time!");
+// }
+
+//logical operators
+//AND(&&)
+// let marks = 75;
+
+// if (marks >= 33 && marks >= 80) {
+//   console.log("Pass");
+//   console.log("A+");
+// }
+
+// OR(||)
+// let marks = 75;
+
+// if (marks >= 33 || marks >= 80) {
+//   console.log("Pass");
+//   console.log("A+");
+// }
+
+// NOT(!)
+// let marks = 75;
+
+// if (!(marks < 33)) {
+//   console.log("Pass");
+//   console.log("A+");
+// }
+
+//PRATICE QUESTION
+// let str = "apple";
+
+// if (str[0] === "a" && str.length > 3) {
+//   console.log("good string");
+// } else console.log("not a good string");
+
+// SWITCH statement
+// let color = "red";
+
+// switch (color) {
+//   case "red":
+//     console.log("Stop");
+//     break;
+//   case "yellow":
+//     console.log("Slow Down");
+//     break;
+//   case "green":
+//     console.log("Go");
+//     break;
+//   default:
+//     console.log("light is broken");
+// }
+
+//PRACTICE question
+// let day = 12;
+
+// switch (day) {
+//   case 1:
+//     console.log("Monday");
+//     break;
+//   case 2:
+//     console.log("Tuesday");
+//     break;
+//   case 3:
+//     console.log("Wednesday");
+//     break;
+//   case 4:
+//     console.log("Thursday");
+//     break;
+//   case 5:
+//     console.log("Friday");
+//     break;
+//   case 6:
+//     console.log("Saturday");
+//     break;
+//   case 7:
+//     console.log("Sunday");
+//     break;
+//   default:
+//     console.log("wrong day");
+// }
+
+let roll = prompt("enter your roll no :");
+console.log(roll);
